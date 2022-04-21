44667c10eff41a167441e1bf7ad9a4f6be804671
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-04-21 16:39:48 -0300

    Realizando exercícios bloco 5.1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1/exercicios/script.js
@@ -0,0 +1,12 @@
+let emergency = document.querySelectorAll(".emergency-tasks h3");
+let noEmergency = document.getElementsByTagName("h3");
+let footer = document.getElementById("footer-container");
+
+footer.style.color = "green";
+
+emergency[0].innerText = "Aprender conteúdo do módulo Desenvolvimento Web sem dúvidas / colocar em dia a máteria sobre Softs Skills";
+emergency[1].innerText = "Corrigir problemas de aúdio no Linux";
+
+noEmergency[2].innerText = "Organizar meu tempo";
+noEmergency[3].innerText = "Jogar Valorant e Assassin's Creed Odyssey";
+
