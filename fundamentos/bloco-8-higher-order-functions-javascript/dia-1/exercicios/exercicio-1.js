0927c1fedb3d3c092ef5df9bc5ff82c491883907
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-05-16 15:30:30 -0300

    bloco 8.1

---
+++ fundamentos/bloco-8-higher-order-functions-javascript/dia-1/exercicios/exercicio-1.js
@@ -0,0 +1,12 @@
+const contratada = (nomeCompleto) => {
+  return `${nomeCompleto.replace(' ', '_').toLowerCase()}@gmail.com`;
+};
+
+const newEmployees = (contratada) => {
+  const employees = {
+    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
---
+++ fundamentos/bloco-8-higher-order-functions-javascript/dia-1/fixando.js
@@ -0,0 +1,17 @@
+const string = () => {
+    return 'Acordando!!';
+}
+
+const cafe = () => {
+    return 'Bora tomar café!!';
+}
+
+const soneca = () => {
+    return 'Partiu dormir!!';
+}
+
+function doingThings(func) {
+    return console.log(func);
+}
+
+doingThings(soneca());
